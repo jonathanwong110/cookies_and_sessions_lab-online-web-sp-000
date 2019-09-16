6a44a776072bd07fc3a158eeadbe70f42d64d3d0
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-16 01:02:14 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' => 'products#i'
-  resources :products, only: [:index]
+  resources :products, only: [:index,]
 end
