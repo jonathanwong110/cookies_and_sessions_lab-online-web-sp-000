5274f482ecba0d16294fcf379b7df99379398c90
Jonathan Wong <jonathan.wong110@gmail.com>
2019-09-16 01:01:43 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'layout' => 'application#cart'
+  get '/' => 'products#i'
   resources :products, only: [:index]
 end
